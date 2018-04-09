b16cf4c805b1c6ee81513a2f276d07110df3e18f
Madhav <kasamsaimadhavk@gmail.com>
2018-04-08 20:24:09 -0400

    Made some changes to test.py and finalSubmission2.csv file.

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ import csv
 csv_file =  'Data/test.csv'
 inputFile = pd.read_csv(csv_file)
 sequence = inputFile['sequence']
-#label = inputFile['label']
 charsPossible = 'ACGT'
 charToInt = dict((c, i) for i, c in enumerate(charsPossible))
 N = len(sequence)
@@ -22,23 +21,6 @@ encoded = to_categorical(intEncoded)
 oneHotEncoded = np.transpose(encoded,(0,2,1))
 oneHotEncodedTest = oneHotEncoded[...,np.newaxis]
 print (oneHotEncoded[0])
-
-
-"""
-
-file = pd.read_csv('Data/test.csv')
-sequence = file['sequence']
-alphabet = 'ACGT'
-char_to_int = dict((c, i) for i, c in enumerate(alphabet))
-N = len(sequence)
-integer_encoded = np.zeros((N,14))
-#print("Starting the one hot encoding process.")
-for i in range(len(sequence)):
-    integer_encoded[i] = [char_to_int[char] for char in sequence[i]]
-encoded = to_categorical(integer_encoded)
-onehotenc = np.transpose(encoded,(0,2,1))
-onehotenctest = onehotenc[...,np.newaxis]    
-"""
 
 print (oneHotEncodedTest.shape)
 
@@ -68,22 +50,10 @@ print (classSeqLabels[0])
 print("Started writing into the finalSubmission.csv file.")
 
 with open('finalSubmission2.csv', 'w') as myfile:
-    #writer = csv.writer(myfile)
-    #writer.writerow(["Id", "Prediction"])
     myfile.write("{},{}\n".format("Id", "Prediction"))
     for i in range(400):
         myfile.write("{},{}\n".format(i, int(classSeqLabels[i])))
         
 myfile.close()   
 print("Finished writing into the finalSubmission.csv file.")   
-    
-# writer=csv.writer(open('submission.csv','wb'))
-# header=['id','prediction']
-# length_list=len(header)
-# i=0
 
-# while i!=length_list :
-    # data=header[i]
-    # print data
-    # i=i+1
-    # writer.writerow(data)
